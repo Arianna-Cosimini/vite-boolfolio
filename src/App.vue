56f036f81f3c5fc406b248c0dc9e2626689aeb55
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-29 18:42:49 +0200

    feat: axios call

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
-<script setup>
-  
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+
+        projects: [],
+
+        baseApiUrl: 'http://127.0.0.1:8000/api',
+      }
+    },
+
+    mounted() {
+      this.apiCall();
+    },
+
+    methods: {
+      
+      apiCall() {
+
+        axios.get(this.baseApiUrl + '/projects').then(res => {
+           this.projects = res.data.results;
+           console.log(this.projects);
+        })
+      }
+    },
+  }
 </script>
 
 <template>
  <div class="container py-5 ">
   <h1>ciao</h1>
+
  </div>
     
 </template>
