70ff0e043e404b096738c9f46df796170a88d841
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-30 15:13:14 +0200

    feat: create homepage

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,22 @@
 <script>
-  import axios from 'axios';
-
-import ProjectItem from './components/ProjectItem.vue';
-
 
   export default {
 
     components: {
-        ProjectItem,
         
     },
 
     data() {
       return {
 
-        projects: [],
-
-        isLoading: false,
-
-        apiLinks: [],
-
-        apiPageNumber: 1,
-
-        baseApiUrl: 'http://127.0.0.1:8000/api',
       }
     },
 
     mounted() {
-      this.apiCall();
+
     },
 
     methods: {
-      
-      apiCall() {
-
-        axios.get(this.baseApiUrl + '/projects', { 
-          params: {
-            page: this.apiPageNumber
-          }
-        }).then(res => {        
-
-           this.projects = res.data.results.data;
-
-           this.apiLinks = res.data.results.links;
-        })
-      },
-
-      changeApiPage(pageNumber) {
-        
-        this.apiPageNumber = pageNumber;
-
-        this.apiCall();
-      },
 
     },
 
@@ -59,68 +24,11 @@ import ProjectItem from './components/ProjectItem.vue';
 </script>
 
 <template>
- <div class="container py-5 ">
-  <h1 class="pb-4">Progetti</h1>
-
-    <div v-if="!isLoading" class="row gap-4" >
-        <ProjectItem
-        class="col-auto"
-        v-for="currentProject in projects"
-        :project="currentProject">
-        </ProjectItem>
-
-        <div id="pagination">
-
-            <ul class="d-flex gap-2">
-        
-                <li v-for="link in apiLinks" v-html="link.label" @click="changeApiPage(link.label)" :class="link.label == apiPageNumber ? 'active' : ''">
-                
-                </li>
-                
-            </ul>
-
-        </div>
-        
-    </div>
-    <div v-else>
-        <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    </div>
-
- </div>
+  
+  <router-view></router-view>
     
 </template>
 
 <style >
-
-#pagination {
-  margin-top: 100px;
-  padding-top: 20px;
-  border-top: solid 1px gray;
-
-  ul {
-
-    list-style-type: none;
-    
-    li {
-      
-      padding: 8px;
-
-      text-decoration: none;
-      color: white;
-
-      transition: all .3s ease;
-
-      cursor: pointer;
-
-      &:hover, &.active {
-        background-color: rgba(255,255,255,0.4);
-        color: black;
-      }
-
-    }
-  }
-}
 
 </style>
