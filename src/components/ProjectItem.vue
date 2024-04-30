f8d6737f854210b16f45693638940a010a38d1bd
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-30 17:19:51 +0200

    fix: control on type field

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -29,7 +29,7 @@ export default {
   <div class="card-body">
     <h5 class="card-title">{{ project.name }}</h5>
     <p class="card-text">{{ project.description }}</p>
-    <p class="card-text">{{ project.type.title }}</p>
+    <p class="card-text" v-if="project.type =! null">{{ project.type.title }}</p>
     <p class="card-text" v-for="technology in project.technologies">{{ technology.title }}</p>
     <router-link :to="{name: 'single-project', params: {slug: project.slug}}" class="btn btn-info">Mostra Progetto</router-link>
   </div>
