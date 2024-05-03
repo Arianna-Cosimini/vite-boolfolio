6368709d342aa2758885674993cb72be23ba0d81
arianna-cosimini <arianna.cosimini@gmail.com>
2024-05-03 16:31:32 +0200

    Fix: aggiunto controllo in methods in ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -29,12 +29,21 @@ export default {
     },
 
     methods: {
-        sendContactRequest(){
+        sendContactRequest() {
 
-            
-            axios.post('http://127.0.0.1:8000/api/newcontact', this.formData ).then(res =>{
+
+            axios.post('http://127.0.0.1:8000/api/newcontact', this.formData).then(res => {
                 console.log(this.formData)
-                
+                if (res.data.success) {
+                    alert('Email inviata con successo!')
+
+                    this.formData = {
+                        name: '',
+                        address: '',
+                        phone: '',
+                        message: ''
+                    };
+                }
             })
 
         }
@@ -66,15 +75,15 @@ export default {
                         <div class="bg-dark border rounded shadow-sm overflow-hidden">
 
                             <!-- form contatti -->
-                            <form  @submit.prevent="sendContactRequest()">
+                            <form @submit.prevent="sendContactRequest()">
                                 <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
 
                                     <!-- name -->
                                     <div class="col-12">
                                         <label for="name" class="form-label">Full Name <span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" id="name" name="name" v-model="formData.name"
-                                            required>
+                                        <input type="text" class="form-control" id="name" name="name"
+                                            v-model="formData.name" required>
                                     </div>
 
                                     <!-- email -->
@@ -89,8 +98,8 @@ export default {
                                                         d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
                                                 </svg>
                                             </span>
-                                            <input type="email" class="form-control" id="address" name="address" v-model="formData.address"
-                                                required>
+                                            <input type="email" class="form-control" id="address" name="address"
+                                                v-model="formData.address" required>
                                         </div>
                                     </div>
 
@@ -106,7 +115,8 @@ export default {
                                                         d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
                                                 </svg>
                                             </span>
-                                            <input type="text" class="form-control" id="phone" name="phone" v-model="formData.phone" >
+                                            <input type="text" class="form-control" id="phone" name="phone"
+                                                v-model="formData.phone">
                                         </div>
                                     </div>
 
@@ -114,8 +124,8 @@ export default {
                                     <div class="col-12">
                                         <label for="message" class="form-label">Message <span
                                                 class="text-danger">*</span></label>
-                                        <textarea class="form-control" id="message" name="message" rows="3" v-model="formData.message"
-                                            required></textarea>
+                                        <textarea class="form-control" id="message" name="message" rows="3"
+                                            v-model="formData.message" required></textarea>
                                     </div>
                                     <div class="col-12">
                                         <div class="d-grid">
